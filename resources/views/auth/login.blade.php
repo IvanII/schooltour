6220007fa442e880acf4cf83b18ac60c8803f81f
IvanII <ivanoviaa92@gmail.com>
2017-12-25 21:12:48 +0600

    Added quickadmin entities

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,65 @@
+@include('admin.partials.header')
+<div style="margin-top: 10%;"></div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Панель администратора</div>
+                <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>{{ trans('quickadmin::auth.whoops') }}</strong> {{ trans('quickadmin::auth.some_problems_with_input') }}
+                            <br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <form class="form-horizontal"
+                          role="form"
+                          method="POST"
+                          action="{{ url('login') }}">
+                        <input type="hidden"
+                               name="_token"
+                               value="{{ csrf_token() }}">
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">E-mail</label>
+
+                            <div class="col-md-6">
+                                <input type="email"
+                                       class="form-control"
+                                       name="email"
+                                       value="{{ old('email') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Пароль</label>
+
+                            <div class="col-md-6">
+                                <input type="password"
+                                       class="form-control"
+                                       name="password">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit"
+                                        class="btn btn-primary"
+                                        style="margin-right: 15px;">
+                                    Войти
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@include('admin.partials.footer')
